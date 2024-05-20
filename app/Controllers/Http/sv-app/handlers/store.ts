374999b5700100b478b35e86732d987c7303fb94
SantiSanGal <santiagon.g.m@gmail.com>
2024-05-20 13:41:30 -0400

    insert grupo de pixeles

--- app/Controllers/Http/sv-app/handlers/store.ts
+++ app/Controllers/Http/sv-app/handlers/store.ts
@@ -1,5 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Database from '@ioc:Adonis/Lucid/Database';
 export const store = async ({ request, response, auth }: HttpContextContract) => {
+
     let params = {
         data: {},
         notification: {
@@ -9,9 +11,46 @@ export const store = async ({ request, response, auth }: HttpContextContract) =>
         }
     }
 
+    const trx = await Database.transaction();
+
     try {
-        return response.json({ message: 'xd oiko\'i' })
+        const { grupo_pixeles, pixeles } = request.all()
+        console.log('pixeles param', pixeles);
+
+        let grupo_pixel_insert_params = {
+            link_adjunta: grupo_pixeles.link,
+            coordenada_x_inicio: grupo_pixeles.coordenada_x_inicio,
+            coordenada_y_inicio: grupo_pixeles.coordenada_y_inicio,
+            coordenada_x_fin: grupo_pixeles.coordenada_x_fin,
+            coordenada_y_fin: grupo_pixeles.coordenada_y_fin,
+            id_datos_compra: 1
+        }
+
+        const [{ id_grupo_pixeles }] = await trx.table('grupos_pixeles').insert(grupo_pixel_insert_params).returning('id_grupo_pixeles')
+        console.log('grupo_pixel_id', id_grupo_pixeles);
+        if (id_grupo_pixeles) {
+            let pixeles_individuales_insert_params = new Array()
+            for (const pixel of pixeles) {
+                console.log('pixel', pixel);
+                pixeles_individuales_insert_params.push({
+                    coordenada_x: pixel.coordenada_x,
+                    coordenada_y: pixel.coordenada_y,
+                    color: pixel.color,
+                    id_grupo_pixeles: id_grupo_pixeles
+                })
+            }
+            await trx.table('pixeles_individuales').insert(pixeles_individuales_insert_params)
+        }
+
+        await trx.commit();
+        params.notification.state = true
+        params.notification.type = 'success'
+        params.notification.message = 'Grupo Registrado Correctamente'
+        return response.json(params)
     } catch (error) {
+        console.log('error', error);
+        
+        await trx.rollback();
         return response.json(params)
     }
 }
