28ede81da438f1fbaf8194e6eba037930ba4efa8
SantiSanGal <santiagon.g.m@gmail.com>
2024-05-26 23:53:25 -0400

    scheduler que genera una imagen con los datos de la bd

--- app/Controllers/Http/sv-app/canvas/handlers/list.ts
+++ app/Controllers/Http/sv-app/canvas/handlers/list.ts
@@ -1,10 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
-import { createCanvas } from 'canvas'
-import path from 'path'
-import fs from 'fs'
 
-export const list = async ({ response, auth }: HttpContextContract) => {
+export const list = async ({ response }: HttpContextContract) => {
     let params = {
         data: {},
         notification: {
@@ -19,37 +16,11 @@ export const list = async ({ response, auth }: HttpContextContract) => {
             .query()
             .from('pixeles_individuales')
 
-        const canvas = createCanvas(2000, 1000)
-        const ctx = canvas.getContext('2d')
-
-        ctx.fillStyle = 'white'
-        ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-        data.forEach(item => {
-            const { coordenada_x, coordenada_y, color } = item
-            console.log('Item:', item)
-
-            // Validar color
-            if (!/^#[0-9A-F]{6}$/i.test(color)) {
-                console.error(`Color inválido: ${color}`)
-                return
-            }
-
-            ctx.fillStyle = color
-            ctx.fillRect(coordenada_x, coordenada_y, 1, 1) // Pintar un pixel en la coordenada
-        })
-
-
-        const buffer = canvas.toBuffer('image/png')
-        const imagePath = path.join(__dirname, '../../../../../../', 'public', 'images', 'output.png')
-        fs.writeFileSync(imagePath, buffer)
-
         params.data = data
         params.notification.state = true
         params.notification.type = 'success'
         params.notification.message = 'Listado Correctamente'
-        response.type('image/png')
-        return response.send(buffer)
+        return response.json(params)
     } catch (e) {
         console.error(e)
         params.notification.message = e.message
--- app/Tasks/GenerarImagen.ts
+++ app/Tasks/GenerarImagen.ts
@@ -1,14 +1,58 @@
-import { BaseTask } from 'adonis5-scheduler/build/src/Scheduler/Task';
+import Database from '@ioc:Adonis/Lucid/Database';
+import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task'
+import { createCanvas } from 'canvas';
+import { DateTime } from 'luxon';
+import path from 'path';
+import fs from 'fs';
 
 export default class GenerarImagen extends BaseTask {
-    public static get schedule() {
-        return '*/1 * * * *';
-    }
-    public static get useLock() {
-        return true;
-    }
-    public async handle(){
-        console.log('hola soy el job');
-        
-    }
+  public static get schedule() {
+    return '0 * * * * *'
+    // return CronTimeV2.everySecond()
+  }
+
+  /**
+   * Set enable use .lock file for block run retry task
+   * Lock file save to `build/tmp/adonis5-scheduler/locks/your-class-name`
+   */
+  public static get useLock() {
+    return false
+  }
+
+  public async handle() {
+    console.log('hola soy el job');
+    const now = DateTime.now()
+    console.log(now.toFormat('yyyy-MM-dd HH:mm:ss'));
+
+    const data = await Database.connection('pg')
+      .query()
+      .from('pixeles_individuales')
+
+    const canvas = createCanvas(2000, 1000)
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    data.forEach(item => {
+      const { coordenada_x, coordenada_y, color } = item
+
+      // Validar color
+      if (!/^#[0-9A-F]{6}$/i.test(color)) {
+        console.error(`Color inválido: ${color}`)
+        return
+      }
+
+      ctx.fillStyle = color
+      ctx.fillRect(coordenada_x, coordenada_y, 1, 1) // Pintar un pixel en la coordenada
+    })
+
+
+    const buffer = canvas.toBuffer('image/png')
+    const imagePath = path.join(__dirname, './../../', 'public', 'images', 'outputScheduler.png')
+    fs.writeFileSync(imagePath, buffer)
+
+    // Remove this promise and insert your code:
+    await new Promise((res) => setTimeout(res, 2000))
+  }
 }
