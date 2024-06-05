9abbf642b957449f28d6c8962231b0d979e5add0
SantiSanGal <santiagon.g.m@gmail.com>
2024-06-04 20:52:05 -0400

    hice una ruta para validar token

--- start/sv-auth/auth.ts
+++ start/sv-auth/auth.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-    Route.post('/login', 'sv-auth/controller.verify')
+    Route.post('/login', 'sv-auth/controller.login')
     Route.post('/register', 'sv-auth/controller.register')
     Route.post('/logout', 'sv-auth/controller.logout')
     Route.get('/verify', 'sv-auth/controller.verify')
